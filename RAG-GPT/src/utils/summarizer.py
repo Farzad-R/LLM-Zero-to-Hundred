3c61dce2abd5e987399fa48766ef06e135ca2cc8
Far-R <farzad.roozitalab@gmail.com>
2024-01-05 08:10:25 -0500

    update rag-gpt summarizer + sub requirements

--- RAG-GPT/src/utils/summarizer.py
+++ RAG-GPT/src/utils/summarizer.py
@@ -53,27 +53,33 @@ class Summarizer:
         full_summary = ""
         counter = 1
         print("Generating the summary..")
-        for i in range(len(docs)):
-            # NOTE: This part can be optimized by considering a better technique for creating the prompt. (e.g: lanchain "chunksize" and "chunkoverlap" arguments.)
-            if i == 0:  # For the first page
-                prompt = docs[i].page_content + \
-                    docs[i+1].page_content[:character_overlap]
-            # For pages except the fist and the last one.
-            elif i < len(docs)-1:
-                prompt = docs[i-1].page_content[-character_overlap:] + \
-                    docs[i].page_content + \
-                    docs[i+1].page_content[:character_overlap]
-            else:  # For the last page
-                prompt = docs[i-1].page_content[-character_overlap:] + \
-                    docs[i].page_content
-            summarizer_llm_system_role = summarizer_llm_system_role.format(
-                max_summarizer_output_token)
+        # if the document has more than one pages
+        if len(docs) > 1:
+            for i in range(len(docs)):
+                # NOTE: This part can be optimized by considering a better technique for creating the prompt. (e.g: lanchain "chunksize" and "chunkoverlap" arguments.)
+
+                if i == 0:  # For the first page
+                    prompt = docs[i].page_content + \
+                        docs[i+1].page_content[:character_overlap]
+                # For pages except the fist and the last one.
+                elif i < len(docs)-1:
+                    prompt = docs[i-1].page_content[-character_overlap:] + \
+                        docs[i].page_content + \
+                        docs[i+1].page_content[:character_overlap]
+                else:  # For the last page
+                    prompt = docs[i-1].page_content[-character_overlap:] + \
+                        docs[i].page_content
+                summarizer_llm_system_role = summarizer_llm_system_role.format(
+                    max_summarizer_output_token)
             full_summary += Summarizer.get_llm_response(
                 gpt_model,
                 temperature,
                 summarizer_llm_system_role,
                 prompt=prompt
             )
+        else:  # if the document has only one page
+            full_summary = docs[0].page_content
+
             print(f"Page {counter} was summarized. ", end="")
             counter += 1
         print("\nFull summary token length:", count_num_tokens(
