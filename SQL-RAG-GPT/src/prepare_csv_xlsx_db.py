92e531bb272cd30e136184ec85edee8a8d1ab904
Farzad-R <farzad.roozitalab@gmail.com>
2024-04-23 22:02:55 -0400

    remove sql chatbot

--- SQL-RAG-GPT/src/prepare_csv_xlsx_db.py
+++
@@ -1,8 +0,0 @@
-from utils.prepare_db_from_csv_xlsx import PrepareSQLFromCSVs
-from utils.load_config import LoadConfig
-
-APPCFG = LoadConfig()
-
-if __name__ == "__main__":
-    prep_sql_instance = PrepareSQLFromCSVs(APPCFG.stored_csv_xlsx_directory)
-    prep_sql_instance.run_pipeline()
